43a177c9290ea92db7d3de92c92ec8bc05cf3f13
davidyallerco <david.yallerco@gmail.com>
2023-11-19 16:36:15 -0500

    first commit

---
+++ src/test/java/net/pe/yallerco/reactor/app/ReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.pe.yallerco.reactor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
